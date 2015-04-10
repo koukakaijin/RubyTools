2d83e9d2f0559abf14177a5582f35fe3c8cd8c8d
koukakaijin <koukakaijin@gmail.com>
2015-04-10 15:09:27 +0200

    DataTools v0.02
    added random string generator

--- src/DataTools.rb
+++ src/DataTools.rb
@@ -13,4 +13,12 @@ class DataTools
 	def mul(*args)
 		args.inject(:*)
 	end
+	#######################################################################
+	# mul
+	#  return a random string of the requested length.
+	#######################################################################
+	def random_string(i=50)
+		return "argument must be an integer" unless i.is_a? Integer
+		(0...i).map { ('a'..'z').to_a[rand(26)] }.join
+	end
 end
